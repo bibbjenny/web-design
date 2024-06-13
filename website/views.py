a9b31a3ef4fb01a4f3a3afb43a06618446f0acb8
bibbjenny <20689@burnside.school.nz>
2024-06-13 19:22:14 +1200

    creating nav bar(in process)

--- website/views.py
+++ website/views.py
@@ -68,43 +68,70 @@ def quiz(id):
 def resources():
     return render_template('resources.html')
 
-@views.route('/forum/<id>/') # why can't have routes without / at the end?
+@views.route('/forum/<id>/', methods=['GET', 'POST']) # 1.
 def forum(id):
     if id == 'home': # shows list of posts with their titles
        sql = 'SELECT * FROM forum_posts;'
        posts = query_db(sql)
+       # 0=id, 1=userID, 2=title, 3=content, 4=date
        if posts: #check if any posts exsist
-           # how to view items in posts... perhaps use table?
            return render_template('forumHome.html', posts=posts)
        else:
            return render_template('forumHome.html', posts='No posts are currently available')
-    else:
-        # id = forum_posts.postID
-        id = int(id)
+    elif id == 'create':
         if request.method == 'GET':
-            sql = 'SELECT * FROM forum_posts WHERE postID = ?;'
-            post_data = query_db(sql, (id,), one = True)
-            # postID = 0, userID = 1, title = 2, content = 3, date = 4
-
-            if not post_data: # post doesn't exist
-                flash('Post not available.', category='error')
-                return redirect(url_for('views.forum', id = 'home'))
-            
-            sql = '''
-            SELECT user.username, user.profile_pic, forum_posts.userID
-            FROM user
-            LEFT JOIN forum_posts on user.userID = forum_posts.userID
-            WHERE forum_posts.postID = ?;
-            '''
-            user = query_db(sql, (id,), one=True)
-            # username = 0, profile pic = 1, userID = 2
-            return render_template('forum.html', title = post_data[2], content = post_data[3], date = post_data[4], username = user[0], profile_pic = user[1])
+            user = session.get('user')
+            if user: # check if user is logged in
+                return render_template('forumCreate.html')
+            else:
+                flash('Login is required', category='error')
+                return redirect(url_for('auth.login'))
         elif request.method == 'POST':
             user = session.get('user')
             # 0 id, 1 email, 2 username, 3 password, 4 profile pic
-            username = user[2]
-            profile_pic = user[4]
-            title = request.form.get('title')
-            content = request.form.get('content')
-            date = request.form.get('date')
-            return render_template('forum.html', title, content, date, username, profile_pic)
+            if user: # check if user is logged in
+                userID = user[0]
+                title = request.form.get('title')
+                content = request.form.get('content')
+                if not title: # needs title
+                    flash('Title can\'t be empty', category='error')
+                    return render_template('forumCreate.html', content=content)
+                if not content: # needs content
+                    flash('Content can\'t be empty', category='error')
+                    return render_template('forumCreate.html', title=title)
+                sql2 = 'SELECT postID FROM forum_posts ORDER BY postID DESC Limit 1;'
+                id = query_db(sql2, (), one=True)
+                if id: # id has to be the next number of the last post
+                    new_id = 1+ int(id[0])
+                else: # if no posts exist, id has to be 1
+                    new_id = 1  
+                sql = 'INSERT INTO forum_posts (postID, userID, title, content) VALUES (?, ?, ?, ?);'
+                query_db(sql, (new_id, userID, title, content), one=True)
+                flash('Post successfully created!', category="success")
+                return redirect(url_for('views.forum', id = new_id ))
+            else:
+                flash('Login is required', category='error')
+                return redirect(url_for('auth.login'))
+    else:
+        # id = forum_posts.postID
+        id = int(id)
+        sql = 'SELECT * FROM forum_posts WHERE postID = ?;'
+        post_data = query_db(sql, (id,), one = True)
+        # postID = 0, userID = 1, title = 2, content = 3, date = 4
+
+        if not post_data: # post doesn't exist
+            flash('Post not available.', category='error')
+            return redirect(url_for('views.forum', id = 'home'))
+        
+        sql = '''
+        SELECT user.username, user.profile_pic, forum_posts.userID
+        FROM user
+        LEFT JOIN forum_posts on user.userID = forum_posts.userID
+        WHERE forum_posts.postID = ?;
+        '''
+        user = query_db(sql, (id,), one=True)
+        # username = 0, profile pic = 1, userID = 2
+        return render_template('forum.html', title = post_data[2], content = post_data[3], date = post_data[4], username = user[0], profile_pic = user[1])
+    
+# @views.route('/comments/<id>', methods=['GET', 'POST'])
+# def comments(id):
